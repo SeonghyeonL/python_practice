bd45f6d2da2bd779dff788ffabc0bfb1b65e6a5c
kate1301@naver.com <kate1301@naver.com>
2023-02-27 17:17:19 +0900

    1000-10869

--- step/one.py
+++ step/one.py
@@ -3,10 +3,51 @@
 # 2557
 
 print('Hello World!')
-"""
+
 
 # 1000
 
+ab = input()
+a = int(ab[0])
+b = int(ab[2])
+print(a+b)
+
+
+# 1001
+
+ab = input()
+a = int(ab[0])
+b = int(ab[2])
+print(a-b)
+
+
+# 10998
+
+ab = input()
+a = int(ab[0])
+b = int(ab[2])
+print(a*b)
+
+
+# 1008
+
+ab = input()
+a = int(ab[0])
+b = int(ab[2])
+print(a/b)
+
+
+# 10869
+
+a, b = map(int, input().split())
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
+"""
+
+# 10926
 
 
 
