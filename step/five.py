89b2e8a6c9c141198bd36fcf9ad63e7f4cd61249
kate1301@naver.com <kate1301@naver.com>
2023-03-01 01:12:07 +0900

    11654-11720

--- step/five.py
+++ step/five.py
@@ -1,6 +1,47 @@
 
-
+"""
 # 11654
+
+import sys
+input = sys.stdin.readline
+given = input().strip()
+print(ord(given))
+
+
+# 2743
+
+import sys
+input = sys.stdin.readline
+given = input().strip()
+print(len(given))
+
+
+# 9086
+
+import sys
+input = sys.stdin.readline
+T = int(input())
+for i in range(T):
+    res = ""
+    given = input().strip()
+    res += given[0]
+    res += given[len(given)-1]
+    print(res)
+
+
+# 11720
+
+import sys
+input = sys.stdin.readline
+N = int(input())
+given = input().strip()
+sum = 0
+for i in range(N):
+    sum += int(given[i])
+print(sum)
+"""
+
+# 10809
 
 import sys
 input = sys.stdin.readline
