ad0e17419b1c5abbe2830d6c52342ef983169ff8
kate1301@naver.com <kate1301@naver.com>
2023-04-26 22:54:39 +0900

    3584,17435

--- step/thirtynine-fourtyfive.py
+++ step/thirtynine-fourtyfive.py
@@ -106,7 +106,7 @@ while len(zero_list) > 0:
     for i in connect[temp]:
         rank[i] -= 1
         if rank[i] == 0: zero_list.append(i)
-"""
+
 
 # -----------------------------------
 # fourty (최소 공통 조상)
@@ -116,6 +116,59 @@ while len(zero_list) > 0:
 
 import sys
 input = sys.stdin.readline
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    connect = [-1] * (N + 1)
+    for _ in range(N - 1):
+        A, B = map(int, input().split())
+        connect[B] = A  # B의 부모가 A
+    A, B = map(int, input().split())
+    parent_A = [A]
+    while True:
+        if connect[A] == -1:
+            break
+        else:
+            A = connect[A]
+            parent_A.append(A)
+    while True:
+        if B in parent_A:
+            print(B)
+            break
+        B = connect[B]
+
+
+# 17435 (sparse table)
+
+import sys
+input = sys.stdin.readline
+m = int(input())
+f = [0] + list(map(int, input().split()))
+dp = [[f[i]] for i in range(m + 1)]
+# 1 ≤ m ≤ 200000 에서 log(m) = 17.609... < 18 이므로 18까지만 해도 됨
+for i in range(1, 19):
+    for j in range(1, m + 1):
+        dp[j].append(dp[dp[j][i - 1]][i - 1])
+Q = int(input())
+for _ in range(Q):
+    n, x = map(int, input().split())
+    for i in range(18, -1, -1):
+        if n >= 1 << i:  # 2^i보다 크면
+            n -= 1 << i  # 2^i을 빼줌
+            x = dp[x][i]  # 0이 될때까지 반복
+    print(x)
+"""
+
+# 11438
+
+import sys
+input = sys.stdin.readline
+N = int(input())
+for _ in range(N):
+    A, B = map(int, input().split())
+M = int(input())
+for _ in range(M):
+    A, B = map(int, input().split())
 
 
 
