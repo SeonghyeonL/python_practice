5651132f1fd6bbe3d07a32e1e5c6d54a14957cf2
kate1301@naver.com <kate1301@naver.com>
2023-05-08 02:05:10 +0900

    1655

--- step/practice_etc.py
+++ step/practice_etc.py
@@ -19,15 +19,32 @@ for i in range(1, N + 1):
         else:  # j < things[i][0]
             dp[i][j] = dp[i - 1][j]
 print(dp[-1][-1])
-"""
+
 
 # 1655
 
 import sys
+import heapq
 input = sys.stdin.readline
 N = int(input())
+left = []
+right = []
 for _ in range(N):
     n = int(input())
+    if len(left) == len(right): heapq.heappush(left, -n)
+    else: heapq.heappush(right, n)
+    if len(right) > 0 and -left[0] > right[0]:
+        maxi = -heapq.heappop(left)
+        mini = heapq.heappop(right)
+        heapq.heappush(left, -mini)
+        heapq.heappush(right, maxi)
+    print(-left[0])
+"""
+
+# 3197
+
+import sys
+input = sys.stdin.readline
 
 
 
